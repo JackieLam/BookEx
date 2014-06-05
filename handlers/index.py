f7d89f32d6b98301256dc63bf407fe21254cf22b
Jackie Lam <jackielin1992@gmail.com>
2014-06-06 00:29:52 +0800

    Change: index.py

--- handlers/index.py
+++ handlers/index.py
@@ -1,10 +1,29 @@
 #coding:utf-8
 
 import tornado.web
-from model.entity import Entity
+from handlers.book import BookHandler
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('helloworld')
+		user = self.get_secure_cookie("user")
+		self.render('index.html',user=user)
 
+class FoundHandler(tornado.web.RequestHandler):
+    def get(self):
+		user = self.get_secure_cookie("user")
+		self.render('SearchResult.html',user=user)
 
+class NotFoundHandler(tornado.web.RequestHandler):
+    def get(self):
+		user = self.get_secure_cookie("user")
+		self.render('SearchNoResult.html', user=user)
+
+class AddBookShowHandler(tornado.web.RequestHandler):
+    def get(self):
+		user = self.get_secure_cookie("user")
+		self.render('addNewBook.html', user=user)
+
+class HomepageHandler(tornado.web.RequestHandler):
+    def get(self):
+		user = self.get_secure_cookie("user")
+		self.render('homePage.html',user=user)
